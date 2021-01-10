2d404467a44ca9a8dfbca899c11184a5ec303fa3
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-01-10 16:56:42 +0100

    minor changes

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 
 double pow2(double x) { return x * x; }
+double pow3(double x) { return x * x * x; }
 
 /**
  * This function calculates the pp total inelastic cross section
@@ -13,7 +14,7 @@ double pow2(double x) { return x * x; }
  * @return inelastic cross section in [mb]
  */
 double sigma_inel(double Tp) {
-  double LX = std::log(Tp / Tp_th);
-  double Threshold = std::max(0., 1. - std::pow(Tp_th / Tp, 1.9));
-  return (Tp > Tp_th) ? (30.7 - 0.96 * LX + 0.18 * pow2(LX)) * std::pow(Threshold, 3.0) : 0;
+  const double LX = std::log(Tp / Tp_th);
+  const double Threshold = std::max(0., 1. - std::pow(Tp_th / Tp, 1.9));
+  return (Tp > Tp_th) ? (30.7 - 0.96 * LX + 0.18 * pow2(LX)) * pow3(Threshold) : 0;
 }
